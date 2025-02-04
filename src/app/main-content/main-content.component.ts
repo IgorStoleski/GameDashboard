de6a374b4b777c7a5880165cc9dbe93fe02a243a
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2025-02-04 22:41:17 +0100

    *

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./header/navbar/navbar.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    DashboardComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
