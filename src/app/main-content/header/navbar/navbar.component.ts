f59e76f6e649c08d08a9a53569967f6f8f9199dd
Stoli <igor.stoleski@gmx.de>
2025-02-05 12:31:44 +0100

    *

--- src/app/main-content/header/navbar/navbar.component.ts
+++ src/app/main-content/header/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { SettingsComponent } from "../../dashboard/settings/settings.component";
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,8 @@ import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     NgbDropdownModule,
     NgbNavModule,
-  ],
+    SettingsComponent
+],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
