5bea050fba63e6d3ae28e4fe314ea4f75fd27e49
Stoli <igor.stoleski@gmx.de>
2025-02-10 12:31:41 +0100

    *

--- src/app/main-content/dashboard/settings/users/users.component.ts
+++ src/app/main-content/dashboard/settings/users/users.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../shared/material/material.module';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
