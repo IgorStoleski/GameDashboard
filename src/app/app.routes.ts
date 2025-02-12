77c81c99dd77e6c732071b1ad503abc74f93ef94
Stoli <igor.stoleski@gmx.de>
2025-02-12 12:23:02 +0100

    some tweaks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { SettingsComponent } from './main-content/dashboard/settings/settings.component';
+import { DashboardComponent } from './main-content/dashboard/dashboard.component';
 
 
 export const routes: Routes = [
@@ -8,6 +9,7 @@ export const routes: Routes = [
     { 
         path: 'main', component: MainContentComponent, children: [        
             { path: 'settings', component: SettingsComponent },
+            { path: 'dashboard', component: DashboardComponent },
         ]
     },
     
--- src/app/main-content/header/navbar/navbar.component.ts
+++ src/app/main-content/header/navbar/navbar.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { SettingsComponent } from "../../dashboard/settings/settings.component";
 import { MaterialModule } from '../../../shared/material/material.module';
 import { RouterLink } from '@angular/router';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     SettingsComponent,
+    DashboardComponent,
     MaterialModule,
     RouterLink,
 ],
