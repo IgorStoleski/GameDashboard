28d5cb9eb9307976970faa7ce805e02b9c7a4650
Stoli <igor.stoleski@gmx.de>
2025-02-18 12:25:49 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { SettingsComponent } from './main-content/dashboard/settings/settings.component';
 import { DashboardComponent } from './main-content/dashboard/dashboard.component';
+import { PlayerComponent } from './main-content/dashboard/player/player.component';
+import { PlaceringComponent } from './main-content/dashboard/placering/placering.component';
 
 
 export const routes: Routes = [
@@ -10,6 +12,8 @@ export const routes: Routes = [
         path: 'main', component: MainContentComponent, children: [        
             { path: 'settings', component: SettingsComponent },
             { path: 'dashboard', component: DashboardComponent },
+            { path: 'player', component: PlayerComponent},
+            { path: 'placering', component: PlaceringComponent},
         ]
     },
     
