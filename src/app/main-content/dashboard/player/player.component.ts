732d52ec6b9bc51578eac6e6aa9a3fa6acc7f950
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2025-03-05 09:17:01 +0100

    change some logic

--- src/app/main-content/dashboard/player/player.component.ts
+++ src/app/main-content/dashboard/player/player.component.ts
@@ -24,6 +24,7 @@ interface Player {
 })
 export class PlayerComponent {
   @Input() player!: Player; // Spieler-Daten von Dashboard
+  players: Player[] = [];
 
   constructor(private backendService: BackendService, private wsService: PlayerWebsocketService) {}
 
@@ -46,5 +47,32 @@ export class PlayerComponent {
         console.error('Fehler beim Speichern:', error);
       }
     );
+  }
+  
+
+
+  ngOnInit() {
+    // Initial Spieler vom Backend holen
+    this.backendService.getPlayers().subscribe((players) => {
+      this.players = players.map((p) => ({
+        ...p,
+        rounds: [
+          p.round_1, p.round_2, p.round_3, p.round_4, p.round_5,
+          p.round_6, p.round_7, p.round_8, p.round_9, p.round_10
+        ]
+      }));
+    });
+
+    // WebSocket-Updates abonnieren
+    this.wsService.getPlayers().subscribe((players) => {
+      console.log("Live-Update erhalten:", players);
+      this.players = players.map((p) => ({
+        ...p,
+        rounds: [
+          p.round_1, p.round_2, p.round_3, p.round_4, p.round_5,
+          p.round_6, p.round_7, p.round_8, p.round_9, p.round_10
+        ]
+      }));
+    });
   }
 }
